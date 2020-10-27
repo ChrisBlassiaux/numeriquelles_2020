49f37ae6335915b53e335b5ec97349c69f56fb2e
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-10-27 20:35:08 +0100

    feat(home): add js to scroll, shrink header with scroll

--- js/nav.js
+++ js/nav.js
@@ -6,8 +6,6 @@ let dropDownEvents = document.querySelector('.events-cities');
 let eventsTitle = document.querySelector('.section-title-events');
 let dropDownStages = document.querySelector('.stages');
 
-console.log(dropDownEvents)
-
 window.addEventListener('resize', function () {
   if (window.innerWidth >= 1200) {
     nav.style.display = 'block';
@@ -25,3 +23,25 @@ burger.addEventListener('click', function () {
     navbar.classList.remove('fixed');
   }
 });
+
+let navbarSections = document.querySelectorAll('.nav .section');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    navbarSections.forEach(element => {
+      element.style.padding = "20px 15px";
+    });
+    dropDownEvents.style.top = "65px";
+    dropDownStages.style.top = "65px";
+    navbar.style.borderBottom = "4px solid #DBEDF8";
+  } else {
+    navbarSections.forEach(element => {
+      element.style.padding = "42px 15px";
+    });
+    dropDownEvents.style.top = "100px";
+    dropDownStages.style.top = "100px";
+    navbar.style.borderBottom = "none";
+  }
+}
